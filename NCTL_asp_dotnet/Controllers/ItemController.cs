2844434c64a4a4a132fdc0c6a211937bfed13af2
athenluffy <31587653+athenluffy@users.noreply.github.com>
2019-02-23 20:28:14 +0530

    gfd

---
+++ NCTL_asp_dotnet/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NCTL_asp_dotnet.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public string Index()
+        {
+            return "This is the item Controller";
+        }
+    }
+}
---
+++ NCTL_asp_dotnet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NCTL_asp_dotnet.MvcApplication" Language="C#" %>
